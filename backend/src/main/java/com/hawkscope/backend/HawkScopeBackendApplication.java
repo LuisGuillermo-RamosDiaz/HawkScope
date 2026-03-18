fb3957c50d214cca0b291e98648906a588845bdf
Rivera Medina Carlos Antonio <carlosrivera040206@gmail.com>
2026-03-18 10:09:55 -0700

    temp: deshabilitar temporalmente la bd para render compile

--- backend/src/main/java/com/hawkscope/backend/HawkScopeBackendApplication.java
+++ backend/src/main/java/com/hawkscope/backend/HawkScopeBackendApplication.java
@@ -3,7 +3,11 @@ package com.hawkscope.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+// TODO: Quita este (exclude = ...) cuando el equipo de base de datos te entregue las credenciales de MySQL reales.
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class HawkScopeBackendApplication {
 
 	public static void main(String[] args) {
