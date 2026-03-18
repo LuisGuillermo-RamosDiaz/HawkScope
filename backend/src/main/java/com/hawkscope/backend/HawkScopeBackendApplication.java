7207cc42d822c775152184d0882567f5c901ce1c
Rivera Medina Carlos Antonio <carlosrivera040206@gmail.com>
2026-03-18 10:18:57 -0700

    fix: usar exclude para evitar fallo en el server

--- backend/src/main/java/com/hawkscope/backend/HawkScopeBackendApplication.java
+++ backend/src/main/java/com/hawkscope/backend/HawkScopeBackendApplication.java
@@ -3,11 +3,11 @@ package com.hawkscope.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-
-// TODO: Quita este (exclude = ...) cuando el equipo de base de datos te entregue las credenciales de MySQL reales.
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+// TODO: Quita este (excludeName = ...) cuando el equipo de base de datos te entregue las credenciales de MySQL reales.
+@SpringBootApplication(excludeName = {
+    "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration",
+    "org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 public class HawkScopeBackendApplication {
 
 	public static void main(String[] args) {
